de2858abc926fe173af285b553e9fd7855718a2e
Jerrie Pelser <jerrie@jerriepelser.com>
2017-09-07 13:27:05 +0700

    Add scope guard and empty approval component

---
+++ timesheets-spa/angular/src/app/approval/approval.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-approval',
+  templateUrl: './approval.component.html',
+  styleUrls: ['./approval.component.css']
+})
+export class ApprovalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
